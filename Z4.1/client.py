0700902c0a9596f0e982e3033d1fd714279602b9
oscarteeninga <oscarteeninga@gmail.com>
2020-04-23 17:21:40 +0200

    Fix 4.1

--- Z4.1/client.py
+++ Z4.1/client.py
@@ -1,23 +1,39 @@
 import os
-from proto import weather_pb2 as Weather
-from proto import weather_pb2_grpc as Weather_grpc
+import weather_pb2 as Weather
+import weather_pb2_grpc as Weather_grpc
 import time
 import grpc
+import threading
 
-def run():
-    with grpc.insecure_channel("localhost:9999") as channel:
-        stub = Weather_grpc.WeatherServiceStub(channel)
-        try:
-            response = stub.subscribe(Weather.SubscribeRequest(city="Amsterdam"))
-            for i in response:
-                print(i)
-        except KeyboardInterrupt:
-            print("KeyboardInterrupt")
-            channel.unsubscribe(close)
-            exit()
+def showForecast(forecast):
+    print("=== " + forecast.city + " " + forecast.datetime + " ===")
+    for weather in forecast.weather:
+        print("Temperature: " + str(weather.temperature))
+        print("Cloudity: " + str(weather.cloudity))
+        print("Humindity: " + str(weather.humindity))
+        print("Wind speed: " + str(weather.wind.speed))
+        print("Wind direction: " + str(weather.wind.direction))
+    print((len(forecast.city) + len(forecast.datetime)+9)*"=")
+    print("")
+
+def run(channel, city, frequency):
+    stub = Weather_grpc.WeatherServiceStub(channel)
+    try:
+        response = stub.subscribe(Weather.SubscribeRequest(city=city, frequency=frequency))
+        for forecast in response:
+            showForecast(forecast)
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+        channel.unsubscribe(close)
+        exit()
 
 def close(channel):
     channel.close()
 
 if __name__ == "__main__":
-    run()    
+    channel = grpc.insecure_channel("localhost:9999")
+    thread1 = threading.Thread(target=run, args=(channel, "Amsterdam", 1, ))
+    thread2 = threading.Thread(target=run, args=(channel, "London", 5, ))
+    thread1.start()
+    time.sleep(0.05)
+    thread2.start()
--- Z4.1/server.py
+++ Z4.1/server.py
@@ -1,33 +1,28 @@
 from concurrent import futures
 import grpc
-import proto.weather_pb2 as Weather
-import proto.weather_pb2_grpc as Weather_grpc
+import weather_pb2 as Weather
+import weather_pb2_grpc as Weather_grpc
 import time
 import datetime
 import threading
 
 class Listener(Weather_grpc.WeatherServiceServicer):
+    def __init__(self, server):
+        self.server = server
 
     def subscribe(self, request, context):
+        addListener(self.server)
         while True:
-            yield Weather.Forecast(
-                datetime=str(datetime.datetime.now()), 
-                city=request.city, 
-                weather=[Weather.Weather(
-                    temperature=10,
-                    humindity=45,
-                    cloudity=Weather.SUNNY,
-                    wind=Weather.Wind(
-                        speed=21,
-                        direction=Weather.NORTH
-                    )
-                )])
-            time.sleep(1)
+            yield getForecast(request.city)
+            time.sleep(request.frequency)
+
+def addListener(server):
+    print("New subscrible!")
+    Weather_grpc.add_WeatherServiceServicer_to_server(Listener(server), server)
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
-    Weather_grpc.add_WeatherServiceServicer_to_server(Listener(), server)
-    Weather_grpc.add_WeatherServiceServicer_to_server(Listener(), server)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1000))
+    Weather_grpc.add_WeatherServiceServicer_to_server(Listener(server), server)
     server.add_insecure_port("[::]:9999")
     server.start()
     try:
@@ -39,11 +34,21 @@ def serve():
         server.stop(0)
 
 def getForecast(city):
-    timedata = datetime.datetime.now()
-    return Weather.Forecast(datatime=timedata, city=city, weather=getWeather())
+    return Weather.Forecast(
+        datetime=str(datetime.datetime.now()).split('.')[0], 
+        city=city, 
+        weather=[getWeather()]
+        )
 
 def getWeather():
-    return Weather.Weather()
+    return Weather.Weather(
+        temperature=10,
+        humindity=45,
+        cloudity=Weather.CLOUDY,
+        wind=Weather.Wind(
+            speed=21,
+            direction=Weather.NORTH
+        ))
 
 if __name__ == "__main__":
     serve()
